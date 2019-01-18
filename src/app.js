a553884f11ff6a1f63635bd2f6f8137214b663d4
Rob <rm.rf.etc@gmail.com>
2019-01-18 12:00:17 -0800

    Add redirect from domain root, in case running on localhost.

--- src/app.js
+++ src/app.js
@@ -3,15 +3,19 @@ import Header from 'src/components/Header'
 import SRIndex from 'src/components/SRIndex'
 import ThreadView from 'src/components/ThreadView'
 import LandingPage from 'src/components/LandingPage'
-import {Router, Route, Switch} from 'react-router-dom'
+import {Router, Route, Switch, Redirect} from 'react-router-dom'
 import history from 'src/logic/router-events'
 import {QueryParamHOC} from 'src/components/HOC'
+import initialState from 'src/constants/initial-state'
 
 
 const Notfound = () => (
 	<div>
 		<h1>Page Not Found</h1>
 	</div>
+)
+const GoToHomePage = () => (
+	<Redirect to={`/code-challenge-react1/subreddit/${initialState.subreddit}/`} />
 )
 
 const App = () => (
@@ -25,6 +29,11 @@ const App = () => (
 		<Header />
 		<Router history={history}>
 			<Switch>
+				<Route
+					exact
+					path="/"
+					component={GoToHomePage}
+				/>
 				<Route
 					exact
 					path="/code-challenge-react1/"
